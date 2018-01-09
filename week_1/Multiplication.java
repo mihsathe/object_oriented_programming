5dc374d234cfcd80137bd13f3dc0572ba1164e0a
Jacky Satpal <jackysatpal.212@gmail.com>
2018-01-08 22:57:13 -0500

    exercise #7

---
+++ week_1/Multiplication.java
@@ -0,0 +1,17 @@
+public class Multiplication {
+
+    public static void main(String[] args) {
+
+        int firstNumber = 1337;
+        int secondNumber = 5050;
+        
+        int result = firstNumber * secondNumber;
+        
+        String output = firstNumber + " * " + secondNumber + " = " + result;
+        
+        System.out.println(output);
+
+        // Program your solution here. Remember to use variables a and b!
+    }
+
+}
