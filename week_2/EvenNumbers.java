bada47467470620608d3193cb166f75c6057e198
Jacky Satpal <jackysatpal.212@gmail.com>
2018-01-08 23:06:19 -0500

    exercise #29

---
+++ week_2/EvenNumbers.java
@@ -0,0 +1,12 @@
+
+public class EvenNumbers {
+
+    public static void main(String[] args) {
+        int initialNumber = 2;
+        
+        while (initialNumber <= 100) {
+            System.out.println(initialNumber);
+            initialNumber = initialNumber + 2;
+        }
+    }
+}
