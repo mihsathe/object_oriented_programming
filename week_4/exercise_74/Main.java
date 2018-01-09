2f58b363fb483272d43128587420fcb0a656b97a
Jacky Satpal <jackysatpal.212@gmail.com>
2018-01-08 23:37:25 -0500

    exercise #74

---
+++ week_4/exercise_74/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        Multiplier threeMultiplier = new Multiplier(3);
+        System.out.println("threeMultiplier.multiply(3): " + threeMultiplier.multiply(2));
+    }
+}
