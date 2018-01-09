6081d7e35b23461d4363a380c4229612372b4aa2
Jacky Satpal <jackysatpal.212@gmail.com>
2018-01-08 23:42:22 -0500

    exercise #79

---
+++ week_4/exercise_79/NumberStatistics.java
@@ -0,0 +1,34 @@
+public class NumberStatistics {
+    
+    private int amountOfNumbers;
+    private int sum;
+    
+    public NumberStatistics() {
+        this.amountOfNumbers = 0;
+        this.sum = 0;
+    }
+    
+    public void addNumber(int number) {
+        this.amountOfNumbers++;
+        this.sum += number;
+    }
+    
+    public int amountOfNumbers() {
+        return this.amountOfNumbers;
+    }
+    
+    public int sum() {
+        return sum;
+    }
+    
+    public double average() {
+        if (sum == 0) {
+            return this.sum;
+        } else {
+            int totalNumbers = this.amountOfNumbers();
+            int sum = this.sum();
+        
+            return (double) sum / totalNumbers;
+        }
+    }
+}
