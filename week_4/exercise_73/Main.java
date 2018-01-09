916816cd44cb346e112bd68cfd1f7fa177a8f69e
Jacky Satpal <jackysatpal.212@gmail.com>
2018-01-08 23:36:10 -0500

    exercise #73 -- Main file

---
+++ week_4/exercise_73/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    
+    public static void main(final String[] args) {
+        Product productA = new Product("Banana", 1.1, 13);
+        
+        productA.printProduct();
+    }
+}
