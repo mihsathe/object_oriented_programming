821760711a8c4fd25fb77e236688774e766c7740
Jacky Satpal <jackysatpal.212@gmail.com>
2018-01-13 18:36:22 -0500

    exercise #105

---
+++ week_6/exercise_105/GuessingGame.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class GuessingGame {
+
+    private Scanner reader;
+
+    public GuessingGame() {
+        this.reader = new Scanner(System.in);
+    }
+
+    public void play(int lowerLimit, int upperLimit) {
+        instructions(lowerLimit, upperLimit);
+
+        while (lowerLimit != upperLimit) {
+            int average = average(lowerLimit, upperLimit);
+            if (isGreaterThan(average)) {
+                lowerLimit = average + 1;
+            } else {
+                upperLimit = average;
+            }
+        }
+        
+        System.out.println("The number you're thinking of is " + lowerLimit);
+    }
+
+    public boolean isGreaterThan(int value) {
+        System.out.println("Is your number greater than " + value + "? (y/n)");
+        return reader.nextLine().equalsIgnoreCase("y");
+    }
+    
+    public int average(int firstNumber, int secondNumber) {
+        return (firstNumber + secondNumber) / 2;
+    }
+
+    public void instructions(int lowerLimit, int upperLimit) {
+        int maxQuestions = howManyTimesHalvable(upperLimit - lowerLimit);
+        System.out.println("Think of a number between " + lowerLimit + "..." + upperLimit + ".");
+        System.out.println("I promise you that I can guess the number you are thinking with " + maxQuestions + " questions.");
+        System.out.println("");
+        System.out.println("Next I'll present you a series of questions. Answer them honestly.");
+        System.out.println("");
+    }
+
+    public static int howManyTimesHalvable(int number) {
+        return (int) (Math.log(number) / Math.log(2)) + 1;
+    }
+}
