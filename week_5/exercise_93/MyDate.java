71cadcc874c960921842022c868d2500aac58a1d
Jacky Satpal <jackysatpal.212@gmail.com>
2018-01-13 18:41:12 -0500

    exercise #93

---
+++ week_5/exercise_93/MyDate.java
@@ -0,0 +1,54 @@
+public class MyDate {
+    private int day;
+    private int month;
+    private int year;
+
+    public MyDate(int day, int montd, int year) {
+        this.day = day;
+        this.month = montd;
+        this.year = year;
+    }
+
+    public String toString() {
+        return this.day + "." + this.month + "." + this.year;
+    }
+
+    public boolean earlier(MyDate compared) {
+        if (this.year < compared.year) {
+            return true;
+        }
+
+        if (this.year == compared.year && this.month < compared.month) {
+            return true;
+        }
+
+        if (this.year == compared.year && this.month == compared.month
+                && this.day < compared.day) {
+            return true;
+        }
+
+        return false;
+    }
+    
+    public int differenceInYears(MyDate comparedDate) {
+         int diff;
+        if (!earlier(comparedDate)) {
+            diff = this.year - comparedDate.year;
+            if (this.month < comparedDate.month) {
+                return diff - 1;
+            } else if (this.month == comparedDate.month) {
+                return this.day < comparedDate.day ? diff - 1 : diff;
+            }
+            return diff;
+        } else {
+            diff = comparedDate.year - this.year;
+            if (comparedDate.month < this.month) {
+                return diff - 1;
+            } else if (comparedDate.month == this.month) {
+                return comparedDate.day < this.day ? diff - 1 : diff;
+            }
+            return diff;
+        }
+    }
+
+}
