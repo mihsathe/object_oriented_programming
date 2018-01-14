48b4d07d1ec94d36ae18dc55590c3f0273ef6a26
Mihir Sathe <msathe@usc.edu>
2018-01-13 18:27:52 -0800

    Update Person.java

--- week_6/exercise_94/Person.java
+++ week_6/exercise_94/Person.java
@@ -1,5 +1,6 @@
 public class Person {
-    
+    // These are not final and there's a method to update one of these.
+    // This means this class is not ThreadSafe
     private String personName;
     private String personNumber;
     
@@ -15,11 +16,11 @@ public class Person {
     public String getNumber() {
         return this.personNumber;
     }
-    
+    // set* is the general convention. Try to avoid this sort of mutability methods unless absolutely needed.
     public void changeNumber(String number) {
         this.personNumber = number;
     }
-    
+    // String.format() is more readable
     public String toString() {
         return this.personName + " number: " + this.personNumber;
     }
